c96fa1047529c099c16ed37905b094976c4814e6
Lu7474 <maxac10112004@mail.com>
2024-11-27 14:00:06 +0300

    fix bag

--- core/routes.py
+++ core/routes.py
@@ -1,7 +1,27 @@
-from flask import Flask, render_template, request
 import requests
+
+from flask import Flask, render_template, request
+
 from core.config import API
 from core import app
+
+
+def get_weather(city):
+    try:
+        url = f"http://api.openweathermap.org/data/2.5/weather"
+        query_params = {"q": city, "appid": API, "units": "metric", "lang": "ru"}
+        response = requests.get(url, params=query_params)
+        data = response.json()
+
+        weather_data = {
+            "city": data["name"],
+            "temperature": data["main"]["temp"],
+            "description": data["weather"][0]["description"],
+        }
+        return weather_data
+    except requests.exceptions.RequestException as e:
+        print(f"Ошибка API: {e}")
+        return None
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -11,14 +31,6 @@ def index():
     if request.method == "POST":
         city = request.form.get("city")
         if city:
-            url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API}&units=metric&lang=ru"
-            response = requests.get(url)
-            data = response.json()
-
-            weather_data = {
-                "city": data["name"],
-                "temperature": data["main"]["temp"],
-                "description": data["weather"][0]["description"],
-            }
+            weather_data = get_weather(city)
 
     return render_template("index.html", weather_data=weather_data)
